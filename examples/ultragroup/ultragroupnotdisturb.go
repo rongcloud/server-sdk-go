83de63b0c113922cb9cfa4cd9e3d40736ba1f10f
hantenglei <hantenglei@rongcloud.cn>
2024-02-26 18:28:24 +0800

    feat: Refactor push/sensitive/ultragroup README.md
    Signed-off-by: hantenglei <hantenglei@rongcloud.cn>

--- examples/ultragroup/ultragroupnotdisturb.go
+++ examples/ultragroup/ultragroupnotdisturb.go
@@ -13,6 +13,7 @@ func main() {
 	rc := rongcloud.NewRongCloud(os.Getenv("APP_KEY"), os.Getenv("APP_SECRET"))
 	ctx := context.Background()
 
+	// 设置群/频道默认免打扰
 	_, err := rc.UltraGroupNotDisturbSet(ctx, &rongcloud.UltraGroupNotDisturbSetRequest{
 		GroupId:     rongcloud.StringPtr("ug01"),
 		BusChannel:  rongcloud.StringPtr("channel01"),
@@ -22,6 +23,7 @@ func main() {
 		log.Fatalf("ultra group not disturb set error %s", err)
 	}
 
+	// 查询默认免打扰配置
 	ultraGroupNotDisturbGetResp, err := rc.UltraGroupNotDisturbGet(ctx, &rongcloud.UltraGroupNotDisturbGetRequest{
 		GroupId:    rongcloud.StringPtr("ug01"),
 		BusChannel: rongcloud.StringPtr("channel01"),
